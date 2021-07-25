fefc90c7fa129498c78e8be800b5816652d84a4a
Joshua Vayer <77019747+joshuaVayer@users.noreply.github.com>
2021-07-25 22:15:49 +0200

    Improvement : add blueprints and controller file (#26)
    * modify files structure
    
    * fix session bug
    
    * Add search functions blueprint
    
    * Add the routes directory
    
    * fix label mistake
    
    Co-authored-by: HNTQ <quentin_henaut@hotmail.fr>

---
+++ routes/auth/auth.py
@@ -0,0 +1,135 @@
+from flask import Blueprint, render_template,session, redirect, request
+import controller as c
+import helpers as h
+
+auth_bp = Blueprint('auth_bp', __name__, template_folder="../../templates", static_folder='../../static')
+
+@auth_bp.route("/login", methods=["GET", "POST"])
+def login():
+    # Forget any user_id
+    session.clear()
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        inputs = {
+            "email": email,
+            "password": password
+        }
+
+        # Ensure form submitted is fully completed
+        form_message = c.form_test(inputs)
+        if form_message:
+            return render_template("login.html", message=form_message)
+
+        # Query database for email
+        query = c.login_db_test(email, password)
+
+        user = query["user"]
+        message = query["message"]
+
+        if message:
+            return render_template("login.html", message=message)
+
+        if not user["active"]:
+            return redirect(url_for("activate", email=email.lower()))
+        # Remember which user has logged in
+        session["user_id"] = user["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+        # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        if request.args.get('message'):
+            message = request.args.get('message')
+        else:
+            message = ""
+        return render_template("login.html", message=message)
+
+@auth_bp.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirmation")
+
+        inputs = {
+            "username": username,
+            "email": email,
+            "password": password,
+            "confirmation": confirm_password
+        }
+
+        # Ensure form submitted is fully completed
+        form_message = c.form_test(inputs)
+        if form_message:
+            return render_template("register.html", message=form_message)
+
+        # Ensure passwords respect minimum requirement and match
+        password_message = c.password_requirement(password, confirm_password)
+        if password_message:
+            return render_template("register.html", message=password_message)
+
+        # Ensure email is not already used
+        db_message = c.register_db_test(email)
+        if db_message:
+            return render_template("register.html", message=db_message)
+
+        # add user in database
+        c.register_db_add(password, username, email)
+
+        return redirect(url_for("activate", email=email.lower()))
+    else:
+        return render_template("register.html")
+
+@auth_bp.route("/activate", methods=["GET", "POST"])
+def activate():
+    if request.method == "POST":
+
+        email = request.form.get("email")
+        confirm_code = request.form.get("confirm")
+
+        inputs = {
+            "email": email,
+            "code": confirm_code
+        }
+        # Ensure form submitted is fully completed
+        form_message = c.form_test(inputs)
+        if form_message:
+            return render_template("activation.html", message=form_message)
+
+        activation_message = c.activation(email, confirm_code)
+        if activation_message:
+            return render_template("activation.html", message=activation_message)
+
+        success_message = "Account activated"
+        return redirect(url_for("login", message=success_message))
+    else:
+        code = ""
+        email = ""
+        if request.args.get('email'):
+            email = request.args.get('email')
+
+        if request.args.get('code'):
+            code = request.args.get('code')
+
+        if code and email:
+            activation_message = c.activation(email, code)
+            if activation_message:
+                return render_template("activation.html", message=activation_message)
+
+            success_message = "Account activated"
+            return redirect(url_for("login", message=success_message))
+        return render_template("activation.html", email=email, code=code)
+
+@auth_bp.route("/logout")
+@h.login_required
+def logout():
+    """Log user out"""
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
---
+++ routes/user/user.py
@@ -0,0 +1,73 @@
+from flask import Blueprint, render_template, request, session
+import controller as c
+import helpers as h
+
+user_bp = Blueprint('user_bp', __name__, template_folder="../../templates", static_folder='../../static')
+
+@user_bp.route("/profile", methods=["GET", "POST"])
+@h.login_required
+def profile():
+    return render_template("profile.html")
+
+
+@user_bp.route("/parameters", methods=["GET", "POST"])
+@h.login_required
+def parameters():
+    email = c.get_email(session["user_id"])
+    if request.method == "POST":
+        if request.form.get("change_email"):
+            new_email = request.form.get("email")
+            password = request.form.get("pass")
+            inputs = {
+                "email": new_email,
+                "password": password
+            }
+
+            # Ensure form submitted is fully completed
+            form_message = c.form_test(inputs)
+            if form_message:
+                return render_template("parameters.html", email=email, email_message=form_message)
+
+            # Query database for email
+            query = c.login_db_test(email, password)
+            message = query["message"]
+            if message:
+                return render_template("parameters.html", email=email, email_message=message)
+
+            # Ensure email is not already used
+            db_message = c.register_db_test(new_email)
+            if db_message:
+                return render_template("parameters.html", email=email, email_message=db_message)
+
+            c.update_email(new_email, session["user_id"])
+            success_message = "Email Updated"
+            return render_template("parameters.html", email=new_email, email_message=success_message)
+
+        elif request.form.get("change_password"):
+            current_password = request.form.get("current")
+            new_password = request.form.get("password")
+            confirm_password = request.form.get("confirmation")
+
+            inputs = {
+                "old password": current_password,
+                "new password": new_password,
+                "confirmation": confirm_password
+            }
+
+            # Ensure form submitted is fully completed
+            form_message = c.form_test(inputs)
+            if form_message:
+                return render_template("parameters.html", email=email, password_message=form_message)
+
+            # Ensure passwords respect minimum requirement and match
+            password_message = c.password_requirement(new_password, confirm_password)
+            if password_message:
+                return render_template("parameters.html", email=email, password_message=password_message)
+
+            c.update_password(new_password, session["user_id"])
+
+            success_message = "Password Updated"
+            return render_template("parameters.html", email=email, password_message=success_message)
+
+    else:
+        return render_template("parameters.html", email=email)
