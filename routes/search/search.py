40eeb3f0b77fe9f854938a1b13c5a416d1498a1f
Joshua Vayer <joshua.vayer@outlook.com>
2021-07-27 22:33:35 +0200

    Add helpers module

--- routes/search/search.py
+++ routes/search/search.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 
-import api.api as api
+import api_client as api
 
 search_bp = Blueprint('search_bp', __name__, template_folder="../../templates", static_folder='../../static')
 
