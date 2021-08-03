bbf01447ef5e088066061a75755fb80ed8e57cc5
Quentin Henaut <quentin_henaut@hotmail.fr>
2021-08-03 21:59:16 +0200

    Improvement: Insert record (#34)
    What news :
        Generate ID if not provide in insert_record function

---
+++ routes/general/__init__.py
@@ -0,0 +1,1 @@
+from .general import general_bp
--- routes/user/user.py
+++ routes/user/user.py
@@ -1,9 +1,13 @@
 from helpers import get_missing_input, password_requirement, login_required
 from flask import Blueprint, render_template, request, session
 from models import User
+import i18n
 
 
-user_bp = Blueprint('user_bp', __name__, template_folder="../../templates", static_folder='../../static')
+user_bp = Blueprint(
+    "user_bp", __name__, template_folder="../../templates", static_folder="../../static"
+)
+
 
 @user_bp.route("/profile", methods=["GET", "POST"])
 @login_required
@@ -19,30 +23,35 @@ def parameters():
         if request.form.get("change_email"):
             new_email = request.form.get("email")
             password = request.form.get("pass")
-            inputs = {
-                "email": new_email,
-                "password": password
-            }
+            inputs = {"email": new_email, "password": password}
 
             # Ensure form submitted is fully completed
             missing_input = get_missing_input(inputs)
             if missing_input:
-                return render_template("parameters.html", email=email, email_message=missing_input)
+                return render_template(
+                    "parameters.html",
+                    email=email,
+                    email_message=i18n.t("missing_input", input=missing_input),
+                )
 
             # Query database for email
             query = User.check_credentials(email, password)
             message = query["message"]
             if message:
-                return render_template("parameters.html", email=email, email_message=message)
+                return render_template(
+                    "parameters.html", email=email, email_message=message
+                )
 
             # Ensure email is not already used
-            db_message = User.is_single_email(new_email)
-            if db_message:
-                return render_template("parameters.html", email=email, email_message=db_message)
+            if not User.email_exist(new_email):
+                return render_template(
+                    "parameters.html", email=email, email_message="Error"
+                )
 
             User.update_email(new_email, session["user_id"])
-            success_message = "Email Updated"
-            return render_template("parameters.html", email=new_email, email_message=success_message)
+            return render_template(
+                "parameters.html", email=new_email, email_message="Success"
+            )
 
         elif request.form.get("change_password"):
             current_password = request.form.get("current")
@@ -52,28 +61,38 @@ def parameters():
             inputs = {
                 "old password": current_password,
                 "new password": new_password,
-                "confirmation": confirm_password
+                "confirmation": confirm_password,
             }
 
             # Ensure form submitted is fully completed
             missing_input = get_missing_input(inputs)
             if missing_input:
-                return render_template("parameters.html", email=email, password_message=missing_input)
+                return render_template(
+                    "parameters.html",
+                    email=email,
+                    password_message=i18n.t("missing_input", input=missing_input),
+                )
 
             query = User.check_credentials(email, current_password)
             message = query["message"]
             if message:
-                return render_template("parameters.html", email=email, password_message=message)
+                return render_template(
+                    "parameters.html", email=email, password_message=message
+                )
 
             # Ensure passwords respect minimum requirement and match
             password_message = password_requirement(new_password, confirm_password)
             if password_message:
-                return render_template("parameters.html", email=email, password_message=password_message)
+                return render_template(
+                    "parameters.html", email=email, password_message=password_message
+                )
 
             User.update_password(new_password, session["user_id"])
 
             success_message = "Password Updated"
-            return render_template("parameters.html", email=email, password_message=success_message)
+            return render_template(
+                "parameters.html", email=email, password_message=success_message
+            )
 
     else:
         return render_template("parameters.html", email=email)
