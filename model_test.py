14874212243db7d4a77c104cda4594ade8d895c4
HNTQ <quentin_henaut@hotmail.fr>
2021-08-03 23:57:47 +0200

    add test for delete_record function

--- model_test.py
+++ model_test.py
@@ -43,11 +43,30 @@ def test_insert_update():
     #test update
     update_record(table_name, {"username":"bar"}, id_test)
 
-    test = get_record(table_name, "id", id_test)
+    first_insert = get_record(table_name, "id", id_test)
+    second_insert = get_record(table_name, "username", "john")
     table_length = sa.Table(table_name, meta, autoload_with=engine)
-    assert test.username == "bar"
-    assert len(table_length.columns) == 2
+    assert first_insert.username == "bar"
+    assert second_insert.username == "john"
 
 
+def test_delete_record():
+    table_name = uuid.uuid4()
+    engine = sa.create_engine(getenv("DATABASE_URL"))
+    meta = sa.MetaData()
+    sa.Table(
+        table_name, 
+        meta,
+        sa.Column("username",
+        sa.String(255)),
+        sa.Column("id",
+        sa.String(255),
+        primary_key=True))
+    meta.create_all(engine)
 
-
+    id_test = str(uuid.uuid4())
+    insert_record(table_name,{"username":"foo", "id": id_test})
+    #test delete function
+    delete_records(table_name, "id", id_test)
+    test = get_record(table_name, "id", id_test)
+    assert not test
