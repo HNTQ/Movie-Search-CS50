03471b4fc46a93a9f6396c58549836e3d00f5405
Joshua Vayer <joshua.vayer@gmail.com>
2021-12-05 22:00:44 +0100

    Feature :  Trending and popular API call

--- api_client/__init__.py
+++ api_client/__init__.py
@@ -1,8 +1,10 @@
 from .api_client import (
-    parse_episodes,
-    global_search,
     query_by_id,
-    parse_query_by_id,
+    global_search,
+    get_index_data,
+    parse_episodes,
+    query_trending,
     query_by_keyword,
-    parse_query_by_keyword,
+    parse_query_by_id,
+    parse_query_by_keyword
 )
